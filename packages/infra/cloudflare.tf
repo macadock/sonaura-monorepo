d755a08ef5228e68b7ed6c8c34a83835ec810c91
Mathieu Barriere <tech@barriere.me>
2024-08-30 12:37:05 +0200

    add terraform config

---
+++ packages/infra/cloudflare.tf
@@ -0,0 +1,39 @@
+terraform {
+  cloud {
+    organization = "Sonaura"
+    workspaces {
+      name = "infra"
+    }
+  }
+  required_providers {
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~>4"
+    }
+  }
+}
+
+variable "cloudflare_api_token" {
+  type = string
+}
+
+variable "zone_id" {
+  default = "b546f34cbd7d6d568a8c82ef9a5b5351"
+}
+
+variable "account_id" {
+  default = "4569021434a99a933cf61a890e6ef2c2"
+}
+
+variable "domain" {
+  default = "sonaura.fr"
+}
+
+provider "cloudflare" {
+  api_token = var.cloudflare_api_token
+}
+
+resource "cloudflare_r2_bucket" "sonaura-r2-marketing-bucket" {
+  name       = "marketing"
+  account_id = var.account_id
+}
