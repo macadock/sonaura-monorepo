013444b9843f672d38e6c47052f26bcb667453c2
Mathieu Barriere <tech@barriere.me>
2024-09-03 15:40:40 +0200

    workflow: add preview env

--- packages/infra/main.tf
+++ packages/infra/main.tf
@@ -1,9 +1,14 @@
 terraform {
-  cloud {
-    organization = "Sonaura"
-    workspaces {
-      name = "infra"
-    }
+  backend "s3" {
+    bucket                      = "sonaura-terraform-state"
+    key                         = "terraform/terraform.tfstate"
+    region                      = "auto"
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+    skip_region_validation      = true
+    skip_requesting_account_id  = true
+    skip_s3_checksum            = true
+    use_path_style              = true
   }
   required_providers {
     cloudflare = {
@@ -17,77 +22,52 @@ variable "cloudflare_api_token" {
   type = string
 }
 
-variable "zone_id" {
-  default = "b546f34cbd7d6d568a8c82ef9a5b5351"
+variable "cloudflare_zone_id" {
+  type = string
 }
 
-variable "account_id" {
-  default = "4569021434a99a933cf61a890e6ef2c2"
+variable "cloudflare_account_id" {
+  type = string
 }
 
-variable "domain-api-staging" {
-  default = "api-staging.sonaura.fr"
+variable "api_url" {
+  type = string
 }
 
-variable "domain-api-production" {
-  default = "api.sonaura.fr"
+variable "website_url" {
+  type = string
 }
 
-variable "domain-marketing-pages" {
-  default = "dev.sonaura.fr"
+variable "environment" {
+  type = string
+}
+
+variable "workspace_name" {
+  type = string
 }
 
 provider "cloudflare" {
   api_token = var.cloudflare_api_token
 }
 
-resource "cloudflare_workers_script" "worker-staging" {
-  account_id         = var.account_id
+resource "cloudflare_workers_script" "sonaura-worker" {
+  account_id         = var.cloudflare_account_id
   content            = file("worker.js")
-  name               = "api-staging"
+  name               = "api-${var.workspace_name}"
   compatibility_date = "2024-08-21"
 }
 
-resource "cloudflare_workers_script" "worker-production" {
-  account_id         = var.account_id
-  content            = file("worker.js")
-  name               = "api-production"
-  compatibility_date = "2024-08-21"
-}
-
-resource "cloudflare_workers_domain" "worker-domain-production" {
-  account_id = var.account_id
-  hostname   = var.domain-api-production
-  service    = cloudflare_workers_script.worker-production.name
-  zone_id    = var.zone_id
-}
-
-resource "cloudflare_workers_domain" "worker-domain-staging" {
-  account_id = var.account_id
-  hostname   = var.domain-api-staging
-  service    = cloudflare_workers_script.worker-staging.name
-  zone_id    = var.zone_id
+resource "cloudflare_workers_domain" "sonaura-worker-domain" {
+  account_id = var.cloudflare_account_id
+  hostname   = var.api_url
+  service    = cloudflare_workers_script.sonaura-worker.name
+  zone_id    = var.cloudflare_zone_id
 }
 
 resource "cloudflare_pages_project" "marketing-pages" {
-  account_id        = var.account_id
+  account_id        = var.cloudflare_account_id
   name              = "marketing"
   production_branch = "main"
-
-  source {
-    type = "github"
-    config {
-      owner                         = "macadock"
-      repo_name                     = "sonaura-monorepo"
-      production_branch             = "main"
-      pr_comments_enabled           = true
-      deployments_enabled           = true
-      production_deployment_enabled = true
-      preview_deployment_setting    = "custom"
-      preview_branch_includes       = ["dev"]
-      preview_branch_excludes       = ["main"]
-    }
-  }
 
   deployment_configs {
     production {
@@ -96,36 +76,20 @@ resource "cloudflare_pages_project" "marketing-pages" {
 
       service_binding {
         name        = "api"
-        service     = cloudflare_workers_script.worker-production.name
+        service     = cloudflare_workers_script.sonaura-worker.name
         environment = "production"
       }
 
       environment_variables = {
-        ENVIRONMENT  = "production"
-        NEXT_API_URL = var.domain-api-production
-      }
-    }
-
-    preview {
-      compatibility_date  = "2024-08-21"
-      compatibility_flags = ["nodejs_compat"]
-
-      service_binding {
-        name        = "api"
-        service     = cloudflare_workers_script.worker-staging.name
-        environment = "staging"
-      }
-
-      environment_variables = {
-        ENVIRONMENT  = "staging"
-        NEXT_API_URL = var.domain-api-staging
+        ENVIRONMENT  = var.environment
+        NEXT_API_URL = var.api_url
       }
     }
   }
 }
 
 resource "cloudflare_pages_domain" "marketing-pages_domain" {
-  account_id   = var.account_id
-  domain       = var.domain-marketing-pages
+  account_id   = var.cloudflare_account_id
+  domain       = var.website_url
   project_name = cloudflare_pages_project.marketing-pages.name
 }
