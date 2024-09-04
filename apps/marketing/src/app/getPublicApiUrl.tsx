46280eb258bc892a29a7f59f2f5229c3c5e4fd5a
Mathieu Barriere <tech@barriere.me>
2024-09-04 18:22:20 +0200

    update main page

--- apps/marketing/src/app/getPublicApiUrl.tsx
+++
@@ -1,9 +0,0 @@
-import { getRequestContext } from "@cloudflare/next-on-pages";
-
-export const runtime = "edge"
-
-export default function getPublicApiUrl() {
-  const { env } = getRequestContext()
-
-  return env.NEXT_PUBLIC_API_URL
-}
