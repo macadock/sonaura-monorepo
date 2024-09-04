fdc8cc746b37788ced81d9b011669ab59bc42ef5
Mathieu Barriere <tech@barriere.me>
2024-09-04 18:28:19 +0200

    update main page

--- apps/marketing/src/app/page.tsx
+++ apps/marketing/src/app/page.tsx
@@ -1,18 +1,9 @@
-import { getRequestContext } from "@cloudflare/next-on-pages";
-
-
-export const runtime = "edge";
-
 export default function Home() {
 
-  const { env } = getRequestContext()
-
-  const nextApiCloudflare = env.NEXT_PUBLIC_API_URL
   const nextApiLocalhost = process.env.NEXT_PUBLIC_API_URL
 
   return (
     <main>
-      <h1>{`getRequestContext: ${env.NEXT_PUBLIC_API_URL}`}</h1>
         <h1>{`process.env: ${nextApiLocalhost}`}</h1>
     </main>
   );
