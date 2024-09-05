d3f2a28dd72336651da5e70d40b368c57f858208
Mathieu Barriere <tech@barriere.me>
2024-09-05 16:58:35 +0200

    add supabase secrets

---
+++ packages/infra/pages.tf
@@ -0,0 +1,36 @@
+resource "cloudflare_pages_project" "marketing-pages" {
+  account_id        = var.CLOUDFLARE_ACCOUNT_ID
+  name              = "marketing-${var.BRANCH_NAME}"
+  production_branch = var.BRANCH_NAME
+  deployment_configs {
+    production {
+      compatibility_date  = "2024-08-21"
+      compatibility_flags = ["nodejs_compat"]
+
+      service_binding {
+        name        = "api"
+        service     = cloudflare_workers_script.sonaura-worker.name
+        environment = "production"
+      }
+
+      environment_variables = {
+        ENVIRONMENT         = var.ENVIRONMENT
+        NEXT_PUBLIC_API_URL = var.API_URL
+      }
+    }
+  }
+}
+
+resource "cloudflare_record" "marketing-pages_domain_zone" {
+  zone_id = var.CLOUDFLARE_ZONE_ID
+  name    = var.WEBSITE_URL
+  content = cloudflare_pages_project.marketing-pages.domains[0]
+  type    = "CNAME"
+  proxied = true
+}
+
+resource "cloudflare_pages_domain" "marketing-pages_domain" {
+  account_id   = var.CLOUDFLARE_ACCOUNT_ID
+  domain       = var.WEBSITE_URL
+  project_name = cloudflare_pages_project.marketing-pages.name
+}
---
+++ packages/infra/workers.tf
@@ -0,0 +1,22 @@
+resource "cloudflare_workers_script" "sonaura-worker" {
+  account_id         = var.CLOUDFLARE_ACCOUNT_ID
+  content            = file("worker.js")
+  name               = "api-${var.BRANCH_NAME}"
+  compatibility_date = "2024-08-21"
+
+  secret_text_binding {
+    name = "SUPABASE_URL"
+    text = var.SUPABASE_URL
+  }
+  secret_text_binding {
+    name = "SUPABASE_ANON_KEY"
+    text = var.SUPABASE_ANON_KEY
+  }
+}
+
+resource "cloudflare_workers_domain" "sonaura-worker-domain" {
+  account_id = var.CLOUDFLARE_ACCOUNT_ID
+  hostname   = var.API_URL
+  service    = cloudflare_workers_script.sonaura-worker.name
+  zone_id    = var.CLOUDFLARE_ZONE_ID
+}
