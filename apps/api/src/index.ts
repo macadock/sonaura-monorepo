ceb4cc83e7b69cc0d8e24e1d5597469d04d1143d
Mathieu Barriere <tech@barriere.me>
2024-09-06 08:38:07 +0200

    chore(api): lint

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,10 +1,9 @@
 import { Hono } from "hono";
-import { pages, categories } from "./routes";
+import { categories, pages } from "./routes";
 
 const app = new Hono<{ Bindings: CloudflareBindings }>().basePath("/api");
 
 app.route("/pages", pages);
 app.route("/categories", categories);
 
-// eslint-disable-next-line import/no-default-export
 export default app;
--- apps/api/src/routes/index.ts
+++ apps/api/src/routes/index.ts
@@ -1,2 +1,2 @@
 export * from "./pages";
-export * from "./categories"
+export * from "./categories";
