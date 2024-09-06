c45f4b84a11255f9803c9ac3124aa40de2d99636
Mathieu Barriere <tech@barriere.me>
2024-09-06 14:48:25 +0200

    chore(api): add installations endpoint

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,10 +1,11 @@
 import { Hono } from "hono";
-import { categories, pages, products } from "./routes";
+import { categories, installations, pages, products } from "./routes";
 
 const app = new Hono<{ Bindings: CloudflareBindings }>().basePath("/api");
 
 app.route("/pages", pages);
 app.route("/categories", categories);
 app.route("/products", products);
+app.route("/installations", installations);
 
 export default app;
